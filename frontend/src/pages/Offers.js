d6fdb620605a37c9d242dedb471b754059044d07
usk0318 <saikiranuppalapati2@gmail.com>
2024-01-11 17:22:07 +0530

    commit

--- frontend/src/pages/Offers.js
+++ frontend/src/pages/Offers.js
@@ -1,12 +1,19 @@
 import React from 'react'
 import Idley from '../assets/Idley'
+import Main from '../assets/Main'
 
 function Offers() {
   return (
     <div className="home-container">
       <p className="title">Best offers for you</p>
       <Idley />
-      
+    <div style={{ marginTop: '50px' }}>
+      <hr></hr>
+      <div style={{ margin: '20px' }}>
+      <p className="title">Order Now to get more offers</p>
+      <Main/>
+      </div>
+      </div>
     </div>
   )
 }
